f6b1fe10bb72fbee52abd7d6ca7d4344e57ff0a3
rawisha <rawvsmaw22@gmail.com>
2025-02-23 10:41:44 +0100

    fix scroll on start screen

--- components/quiz-game/start-screen.tsx
+++ components/quiz-game/start-screen.tsx
@@ -7,10 +7,13 @@ interface StartScreenProps {
 
 export function StartScreen({ categories, onSelectCategory }: StartScreenProps) {
   return (
-    <div className="w-full h-screen md:h-auto  md:max-w-4xl bg-indigo-950 md:rounded-3xl shadow-2xl overflow-hidden overflow-y-auto">
-      <div className="p-8">
-        <h1 className="text-4xl font-bold text-white text-center mb-8">Quiz<span className='text-red-500'>i</span></h1>
-        <p className="text-gray-300 text-center mb-12">Välj en kategori för att börja spela</p>
+    <div className="w-full h-screen md:h-auto md:max-w-4xl bg-indigo-950 md:rounded-3xl shadow-2xl flex flex-col">
+      <div className='p-8'>
+      <h1 className="text-4xl font-bold text-white text-center mb-8">Quiz<span className='text-red-500'>i</span></h1>
+      <p className="text-gray-300 text-center">Välj en kategori för att börja spela</p>
+      </div>
+      
+      <div className="flex-1 overflow-y-auto p-8">
         
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {categories.map((category) => (
