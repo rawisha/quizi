4acc6032fc77871c4a9ffd0d1b3aad249d11673a
rawisha <rawvsmaw22@gmail.com>
2025-02-23 11:10:00 +0100

    fixes

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-02-21'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-02-23'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
